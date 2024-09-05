71459e3c4f2b0cdeb2172ba78ad99f1990a7c015
Paolo Emilio Mazzon <p.mazzon@irpcds.org>
2024-09-05 11:53:36 +0200

    update preview

---
+++ do-preview.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo
+figlet -w 200 -f ./blodro << eot
+ \`~1!2@3#4$5%6^7&8*9(0)-_=+
+ QWERTYUIOPASD
+ FGHJKLZXCVBNM
+ qwertyuiopasd[{]}\|
+ fghjklzxcvbnm;:\'",<.>/?
+eot
+
+figlet -w 200 -f ./blodrobold << eot
+ \`~1!2@3#4$5%6^7&8*9(0)-_=+
+ QWERTYUIOPASD
+ FGHJKLZXCVBNM
+ qwertyuiopasd[{]}\|
+ fghjklzxcvbnm;:\'",<.>/?
+eot
+echo
