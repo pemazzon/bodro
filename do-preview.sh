429733489a2659e904193e60f3189041c4433226
Paolo E. Mazzon <paoloemilio.mazzon@gmail.com>
2024-09-06 11:17:24 +0200

    icorrected preview script. updated preview

--- do-preview.sh
+++ do-preview.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo
-figlet -w 200 -f ./blodro << eot
- \`~1!2@3#4\$5%6^7&8*9(0)-_=+
- QWERTYUIOPASD
+figlet -w 200 -f ./bodro << eot
+ \`~1!2@3#4\$5%6^7&8*9(0)
+ QWERTYUIOPASD -_=+
  FGHJKLZXCVBNM
  qwertyuiopasd[{]}\|
- fghjklzxcvbnm;:\'",<.>/?
+ fghjklzxcvbnm;:'",<.>/?
 eot
 
-figlet -w 200 -f ./blodrobold << eot
- \`~1!2@3#4\$5%6^7&8*9(0)-_=+
- QWERTYUIOPASD
+figlet -w 200 -f ./bodrobold << eot
+ \`~1!2@3#4\$5%6^7&8*9(0)
+ QWERTYUIOPASD -_=+
  FGHJKLZXCVBNM
  qwertyuiopasd[{]}\|
- fghjklzxcvbnm;:\'",<.>/?
+ fghjklzxcvbnm;:'",<.>/?
 eot
 echo
