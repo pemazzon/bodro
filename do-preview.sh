9729f005c38cfcf7e0ade871b8ea160f7afbaf4d
Paolo Emilio Mazzon <p.mazzon@irpcds.org>
2024-09-09 11:27:13 +0200

    modified to create separate previews

--- do-preview.sh
+++ do-preview.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+font=bodro
+
+[ "$1" = "b" ] && font=bodrobold
+[ "$1" = "2" ] && font=dobodro
+
 echo
-figlet -w 200 -f ./bodro << eot
+figlet -w 200 -f ./$font << eot
  \`~1!2@3#4\$5%6^7&8*9(0)
  QWERTYUIOPASD -_=+
  FGHJKLZXCVBNM
@@ -9,11 +14,6 @@ figlet -w 200 -f ./bodro << eot
  fghjklzxcvbnm;:'",<.>/?
 eot
 
-figlet -w 200 -f ./bodrobold << eot
- \`~1!2@3#4\$5%6^7&8*9(0)
- QWERTYUIOPASD -_=+
- FGHJKLZXCVBNM
- qwertyuiopasd[{]}\|
- fghjklzxcvbnm;:'",<.>/?
-eot
-echo
+sleep 2
+
+scrot -u -o ${font}-preview.png
